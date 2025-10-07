aa624faadb49b067bfebd47392849ffc331d2253
Bharath Kumar <t.bharathkumarr@gmail.com>
2025-10-07 22:29:02 +0530

    Done with basic routing and shadcn setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react"
-import "./globals.css"
+import React from "react";
+import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 const App = () => {
-  return <div className="font-bold text-3xl underline">App</div>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-in" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout/>}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
