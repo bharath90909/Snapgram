ba1c47715149224203ee6357912d09dac6b83d1f
Bharath Kumar <t.bharathkumarr@gmail.com>
2025-10-08 00:21:45 +0530

    refactor(sign in form): add default values in zod validator

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -21,7 +21,10 @@ const SignupForm = () => {
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: "",
       username: "",
+      email: "",
+      password: "",
     },
   });
 
