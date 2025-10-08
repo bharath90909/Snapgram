60ac6157183bd95af8d1c513549d6fe5d1d9032c
Bharath Kumar <t.bharathkumarr@gmail.com>
2025-10-08 23:48:17 +0530

    Done with creating a testing user

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -15,6 +15,7 @@ import { Input } from "@/components/ui/input";
 import { SignupValidation } from "@/lib/validation";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
   const isLoading = false;
@@ -28,8 +29,10 @@ const SignupForm = () => {
     },
   });
 
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    console.log("Values: ", values);
+    const newUser = await createUserAccount(values);
+    console.log("New User: ", newUser);
   }
 
   return (
